a44acda9ae19fc9e2744607f64f4a0f937e22906
PavelM87 <mospv87@gmail.com>
2023-01-05 00:05:36 +0400

    Logger. Basic query optimisations

--- service/services/views.py
+++ service/services/views.py
@@ -1,9 +1,15 @@
+from django.db.models import Prefetch
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from services.models import Subscription
 from services.serializers import SubscriptionSerializer
+from clients.models import Client
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    # queryset = Subscription.objects.all().prefetch_related('client').prefetch_related('client__user')
+    queryset = Subscription.objects.all().prefetch_related(
+        'plan',
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name','user__email'))
+    )
     serializer_class = SubscriptionSerializer
